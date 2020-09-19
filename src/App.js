18dabe6ab56926f69fc78ccfcd3c6497337d1ce1
marcio <marcio.tadeu2@gmail.com>
2020-09-18 21:52:59 -0300

    Importanto css e imagens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react'
-import Header from './components/Header'
+import './App.css';
+import img from './assets/img.jpg';
+import Header from './components/Header';
+
 
 function App() {
     const [projects,setProject] = useState(['Desenvolvimento de app','Front-end web']);
@@ -8,6 +11,7 @@ function App() {
     }
     return(
         <>
+            <img src={img} width={300} alt=""/>
             <Header title="Homepage"/>
                 <ul>
                     {projects.map(project => <li key={project}>{project}</li>)}
